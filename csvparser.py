6211719985b1d918275910b10289ff1e37527d42
Jeff Gough <j@jeffreygough.com>
2023-02-23 21:53:59 +0000

    Refactor the CSV parsing function into its own module

---
+++ csvparser.py
@@ -0,0 +1,45 @@
+from transaction import Transaction
+import csv
+from datetime import datetime
+from operator import itemgetter
+
+def parse(filename):
+    """ Parse a CSV file and return a list of transactions it contains """
+
+    data = []
+    transactions = []
+
+    with open(filename, newline='') as csvfile:
+        reader = csv.DictReader(
+                csvfile,
+                fieldnames=("date", "transaction_type", "sort_code", "account_number", "counterparty", "amount_out", "amount_in", "balance"))
+        for row in reader:
+            try:
+                row["date"] = datetime.strptime(row["date"], "%d/%m/%Y").date()     # Convert date string to date object
+                data.append(row)
+            except ValueError:
+                pass
+
+    for row in data:            # Convert strings to floats, if they're valid
+        try:
+            row["amount_in"] = float(row["amount_in"])
+        except ValueError:
+            row["amount_in"] = 0
+            pass
+        try:
+            row["amount_out"] = float(row["amount_out"])
+        except ValueError:
+            row["amount_out"] = 0
+            pass
+        try:
+            row["balance"] = float(row["balance"])
+        except ValueError:
+            row["balance"] = 0
+            pass
+
+    data = sorted(data, key=itemgetter('date'))
+    for row in data:
+        t = Transaction(row["date"], row["transaction_type"], row["counterparty"], row["amount_out"], row["amount_in"], row["balance"])
+        transactions.append(t)
+
+    return transactions
--- spendy.py
+++ spendy.py
@@ -1,13 +1,15 @@
 import argparse
-import csv
 from datetime import datetime
-from operator import itemgetter
 import calendar
+from thefuzz import fuzz
+
+import csvparser
 
 parser = argparse.ArgumentParser(
     prog = "Spendy",
     description = "Tool to parse Lloyds CSV exports and produce insights" )
 
+#parser.add_argument("command", choices=["cluster"])
 parser.add_argument("filename")
 parser.add_argument("--print", action="store_true")
 #parser.add_argument("--total_out", action="store_true")
@@ -21,71 +23,7 @@ parser.add_argument("--months", action="store_true", help="Print all months for 
 args = parser.parse_args()
 data = []
 
-class Transaction:
-    def __init__(self, date, transaction_type, counterparty, amount_out, amount_in, balance):
-        self.date = date
-        self.transaction_type = transaction_type
-        self.counterparty = counterparty
-        self.amount_out = amount_out
-        self.amount_in = amount_in
-        self.balance = balance
-
-    def __str__(self):
-        return "{}\t{}\t{:20}\t{:.2f}\t{:.2f}\t{:.2f}".format(self.date, self.transaction_type, self.counterparty, self.amount_out, self.amount_in, self.balance)
-        
-
-class Transactions:
-
-    def __init__(self, filename):
-        self.data = []
-        self.transactions = []
-
-        with open(filename, newline='') as csvfile:
-            self.reader = csv.DictReader(csvfile, fieldnames=("date", "transaction_type", "sort_code", "account_number", "counterparty", "amount_out", "amount_in", "balance"))
-            for row in self.reader:
-                try:
-                    row["date"] = datetime.strptime(row["date"], "%d/%m/%Y").date()     # Convert date string to date object
-                    self.data.append(row)
-                except ValueError:
-                    pass
-    
-        for row in self.data:            # Convert strings to floats, if they're valid
-            try:
-                row["amount_in"] = float(row["amount_in"])
-            except ValueError:
-                row["amount_in"] = 0
-                pass
-            try:
-                row["amount_out"] = float(row["amount_out"])
-            except ValueError:
-                row["amount_out"] = 0
-                pass
-            try:
-                row["balance"] = float(row["balance"])
-            except ValueError:
-                row["balance"] = 0
-                pass
-
-        self.data = sorted(self.data, key=itemgetter('date'))
-        for row in self.data:
-            t = Transaction(row["date"], row["transaction_type"], row["counterparty"], row["amount_out"], row["amount_in"], row["balance"])
-            self.transactions.append(t)
-
-    def print(self):
-        for t in self.transactions:
-            print(t)
-
-    def get_all(self):
-        return self.transactions
-
-    def get_date_range(self, start, end):
-        return [t for t in self.transactions if t.date >= start and t.date <= end]
-
-txns = Transactions(args.filename)
-
-
-def print_transaction(txn):
-    print("{}\t{}\t{:20}\t{:.2f}\t{:.2f}\t{:.2f}".format(txn["date"], txn["transaction_type"], txn["counterparty"], txn["amount_out"], txn["amount_in"], txn["balance"]))
+txns = csvparser.parse(args.filename)
 
 start = None
 end = None
@@ -101,36 +39,72 @@ if args.year and args.month:
 # Extract subset of transactions according to date range, using list comprehension
 #txns = [d for d in data if d["date"] >= start and d["date"] <= end]
 if start and end:
-    subset = txns.get_date_range(start, end)
+    subset = transactions.within_date_range(txns, start, end)
 
 else:
-    subset = txns.get_all()
+    subset = txns
 
 if args.print:
     for t in subset:
         print(t)
 
-exit(0)
-
-def total_out():
+def total_out(txns):
     out = 0
     for t in txns:
-        out = out + t["amount_out"]
+        out = out + t.amount_out
     return out
 
-def total_in():
+def total_in(txns):
     tin = 0
     for t in txns:
-        tin = tin + t["amount_in"]
+        tin = tin + t.amount_in
     return tin
 
-print("From {} to {} Total in {:.2f} Total out {:.2f}".format(start, end, total_in(), total_out()))
+print("From {} to {} Total in {:.2f} Total out {:.2f}".format(start, end, total_in(subset), total_out(subset)))
+
 
 
 class Counterparties:
-    cp = []
+    def __init__(self, transactions):
+        self.cps = set()           # use a set type to maintain uniqueness
+        for t in transactions:
+            self.add(t.counterparty)
 
     def add(self, name):        # Name is taken directly from the counterparty field in the CSV
-        if not name in cp:
-            cp.append(name);
-        
+        self.cps.add(name);
+
+c = Counterparties(subset)
+
+data = c.cps
+
+threshold     =60 
+minGroupSize = 1
+
+from itertools import combinations
+
+paired = { c:{c} for c in data }
+for a,b in combinations(data,2):
+    if fuzz.partial_ratio(a, b) < threshold: continue
+    paired[a].add(b)
+    paired[b].add(a)
+
+groups    = list()
+ungrouped = set(data)
+while ungrouped:
+    bestGroup = {}
+    for city in ungrouped:
+        g = paired[city] & ungrouped
+        for c in g.copy():
+            g &= paired[c] 
+        if len(g) > len(bestGroup):
+            bestGroup = g
+    if len(bestGroup) < minGroupSize : break  # to terminate grouping early change minGroupSize to 3
+    ungrouped -= bestGroup
+    groups.append(bestGroup)
+
+groups_ord = []
+for g in groups:
+    groups_ord.append(list(g))
+
+groups_ord.sort()
+print(groups_ord)
