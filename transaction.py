7e65f5c93a0d60ed2bbd38a28bd175ea88ea8b9d
Jeff Gough <j@jeffreygough.com>
2023-02-23 22:55:56 +0000

    Rename transaction.counterparty to .transaction_ref to disambiguate the two terms. Thinking more clearly, a counterparty represents a group of transaction references, tagged with a name and maybe some other stuff

--- transaction.py
+++ transaction.py
@@ -1,14 +1,14 @@
 class Transaction:
-    def __init__(self, date, transaction_type, counterparty, amount_out, amount_in, balance):
+    def __init__(self, date, transaction_type, transaction_ref, amount_out, amount_in, balance):
         self.date = date
         self.transaction_type = transaction_type
-        self.counterparty = counterparty
+        self.transaction_ref = transaction_ref
         self.amount_out = amount_out
         self.amount_in = amount_in
         self.balance = balance
 
     def __str__(self):
-        return "{}\t{}\t{:20}\t{:.2f}\t{:.2f}\t{:.2f}".format(self.date, self.transaction_type, self.counterparty, self.amount_out, self.amount_in, self.balance)
+        return "{}\t{}\t{:20}\t{:.2f}\t{:.2f}\t{:.2f}".format(self.date, self.transaction_type, self.transaction_ref, self.amount_out, self.amount_in, self.balance)
 
 def within_date_range(transactions, start, end):
     """ Given a list of transactions, return the subset between dates start and end
