ae1990f11983ddea6a7bc3eaca68260687e7d29c
Jeff Gough <j@jeffreygough.com>
2023-02-24 21:53:49 +0000

    Apply style guide

--- csvparser.py
+++ csvparser.py
@@ -3,6 +3,7 @@ import csv
 from datetime import datetime
 from operator import itemgetter
 
+
 def parse(filename):
     """ Parse a CSV file and return a list of transactions it contains """
 
@@ -11,16 +12,21 @@ def parse(filename):
 
     with open(filename, newline='') as csvfile:
         reader = csv.DictReader(
-                csvfile,
-                fieldnames=("date", "transaction_type", "sort_code", "account_number", "counterparty", "amount_out", "amount_in", "balance"))
+            csvfile,
+            fieldnames=(
+                "date", "transaction_type", "sort_code", "account_number",
+                "counterparty", "amount_out", "amount_in", "balance"
+            )
+        )
         for row in reader:
             try:
-                row["date"] = datetime.strptime(row["date"], "%d/%m/%Y").date()     # Convert date string to date object
+                # Convert date string to date object
+                row["date"] = datetime.strptime(row["date"], "%d/%m/%Y").date()
                 data.append(row)
             except ValueError:
                 pass
 
-    for row in data:            # Convert strings to floats, if they're valid
+    for row in data:      # Convert strings to floats, if they're valid
         try:
             row["amount_in"] = float(row["amount_in"])
         except ValueError:
@@ -39,7 +45,10 @@ def parse(filename):
 
     data = sorted(data, key=itemgetter('date'))
     for row in data:
-        t = Transaction(row["date"], row["transaction_type"], row["counterparty"], row["amount_out"], row["amount_in"], row["balance"])
+        t = Transaction(
+            row["date"], row["transaction_type"], row["counterparty"],
+            row["amount_out"], row["amount_in"], row["balance"]
+        )
         transactions.append(t)
 
     return transactions
--- spendy.py
+++ spendy.py
@@ -6,19 +6,43 @@ import csvparser
 import counterparty
 
 parser = argparse.ArgumentParser(
-    prog = "Spendy",
-    description = "Tool to parse Lloyds CSV exports and produce insights" )
+    prog="Spendy",
+    description="Tool to parse Lloyds CSV exports and produce insights"
+)
 
-#parser.add_argument("command", choices=["cluster"])
+# parser.add_argument("command", choices=["cluster"])
 parser.add_argument("filename")
 parser.add_argument("--print", action="store_true")
-#parser.add_argument("--total_out", action="store_true")
-#parser.add_argument("--total_in", action="store_true")
-parser.add_argument("--start_date", "-s", help="Start date to filter a subset of transactions. Defaults to first transaction if not set. Supply in a valid ISO format")
-parser.add_argument("--end_date", "-e", help="End date to filter a subset of transactions. Defaults to last transaction if not set. Supply in a valid ISO format")
-parser.add_argument("--year", help="Select just one year to filter, or the year to accompany a month.")
-parser.add_argument("--month", help="Select just one month to filter. Requires --year also to be specified")
-parser.add_argument("--months", action="store_true", help="Print all months for the year")
+# parser.add_argument("--total_out", action="store_true")
+# parser.add_argument("--total_in", action="store_true")
+parser.add_argument("--start_date", "-s",
+                    help=(
+                        "Start date to filter a subset of transactions. "
+                        "Defaults to first transaction if not set. Supply "
+                        "in a valid ISO format"
+                    )
+                    )
+parser.add_argument("--end_date", "-e",
+                    help=(
+                        "End date to filter a subset of transactions. "
+                        "Defaults to last transaction if not set. Supply "
+                        "in a valid ISO format"
+                    )
+                    )
+parser.add_argument("--year",
+                    help=(
+                        "Select just one year to filter, or the "
+                        "year to accompany a month."
+                    )
+                    )
+parser.add_argument("--month",
+                    help=(
+                        "Select just one month to filter. "
+                        "Requires --year also to be specified"
+                    )
+                    )
+parser.add_argument("--months", action="store_true",
+                    help="Print all months for the year")
 
 args = parser.parse_args()
 data = []
@@ -29,17 +53,24 @@ start = None
 end = None
 # Only year supplied
 if args.year and args.month is None:
-    start = datetime.strptime("{}-01-01".format(args.year), "%Y-%m-%d").date()  
-    end = datetime.strptime("{}-12-31".format(args.year), "%Y-%m-%d").date()  
+    start = datetime.strptime("{}-01-01".format(args.year), "%Y-%m-%d").date()
+    end = datetime.strptime("{}-12-31".format(args.year), "%Y-%m-%d").date()
 
 if args.year and args.month:
-    start = datetime.strptime("{}-{}-01".format(args.year, args.month), "%Y-%m-%d").date()  
-    end = datetime.strptime("{}-{}-{}".format(args.year, args.month, calendar.monthrange(int(args.year), int(args.month))[1]), "%Y-%m-%d").date()  
+    start = datetime.strptime(
+        "{}-{}-01".format(args.year, args.month), "%Y-%m-%d").date()
+    end = datetime.strptime(
+        "{}-{}-{}".format(args.year, args.month,
+                          calendar.monthrange(
+                              int(args.year),
+                              int(args.month))[1]), "%Y-%m-%d").date()
 
-# Extract subset of transactions according to date range, using list comprehension
-#txns = [d for d in data if d["date"] >= start and d["date"] <= end]
+# Extract subset of transactions according to date range,
+# using list comprehension
+# txns = [d for d in data if d["date"] >= start and d["date"] <= end]
+
 if start and end:
-    subset = transactions.within_date_range(txns, start, end)
+    subset = txns.within_date_range(txns, start, end)
 else:
     subset = txns
 
@@ -47,11 +78,13 @@ if args.print:
     for t in subset:
         print(t)
 
+
 def total_out(txns):
     out = 0
     for t in txns:
         out = out + t.amount_out
     return out
+
 
 def total_in(txns):
     tin = 0
@@ -59,7 +92,8 @@ def total_in(txns):
         tin = tin + t.amount_in
     return tin
 
-print("From {} to {} Total in {:.2f} Total out {:.2f}".format(start, end, total_in(subset), total_out(subset)))
+
+print("From {} to {} Total in {:.2f} Total out {:.2f}".format(
+    start, end, total_in(subset), total_out(subset)))
 
 counterparty.from_transactions(txns)
-
